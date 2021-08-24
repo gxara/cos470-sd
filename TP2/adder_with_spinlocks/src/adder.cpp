29fe95b68e825a9d608d16d14f0f2512b0384662
= <gabrielxara@gmail.com>
2021-08-24 06:59:52 -0300

    Enables to execute the program recursive times and measure the average performance

--- TP2/adder_with_spinlocks/src/adder.cpp
+++ TP2/adder_with_spinlocks/src/adder.cpp
@@ -7,7 +7,7 @@
 #include "../../helpers/splitVector.h"
 using namespace std;
 
-#define NUM_ELEMENTS 100
+#define NUM_ELEMENTS 10000000
 
 atomic_flag lock_stream = ATOMIC_FLAG_INIT;
 
@@ -53,42 +53,59 @@ int main(int argc, char *argv[])
         return 0;
     }
 
-    vector<int> numbers = populate(NUM_ELEMENTS);
+    int numberOfExecutions = 10;
+    double totalTime = 0;
     int numberOfThreads = atoi(argv[1]);
-    int chunkSize = NUM_ELEMENTS / numberOfThreads;
 
-    vector<vector<int>> vectorOfChunks = splitVectorIntoChunks(numbers, chunkSize);
-
-    vector<thread> v;
-
-    // Starting to measure time
-    auto start = chrono::system_clock::now();
-
-    for (int i = 0; i < numberOfThreads; i++)
+    for (int iteration = 1; iteration <= numberOfExecutions; iteration++)
     {
-        vector<int> chunk = vectorOfChunks[i];
 
-        // https://www.cplusplus.com/reference/thread/thread/
-        thread thread_n(acumulate, chunk, i);
-        v.push_back((thread &&)(thread_n));
+        vector<int> numbers = populate(NUM_ELEMENTS);
+        int chunkSize = NUM_ELEMENTS / numberOfThreads;
+
+        vector<vector<int>> vectorOfChunks = splitVectorIntoChunks(numbers, chunkSize);
+
+        vector<thread> v;
+
+        // Starting to measure time
+        auto start = chrono::system_clock::now();
+
+        for (int i = 0; i < numberOfThreads; i++)
+        {
+            vector<int> chunk = vectorOfChunks[i];
+
+            // https://www.cplusplus.com/reference/thread/thread/
+            thread thread_n(acumulate, chunk, i);
+            v.push_back((thread &&)(thread_n));
+        }
+
+        for (int i = 0; i < numberOfThreads; i++)
+        {
+            // Must wait for each thread to finish
+            v[i].join();
+        }
+
+        // Ending to measure time
+        auto end = chrono::system_clock::now();
+
+        chrono::duration<double> elapsed = end - start;
+
+        totalTime = totalTime + elapsed.count();
+
+        cout << "\n************ ITERATION " << iteration << " ************\n"
+             << endl;
+        cout << "Number of elements: " << NUM_ELEMENTS << endl;
+        cout << "Cumulative sum: " << cumulativeSum << endl;
+        cout << "Threads quantity: " << numberOfThreads << endl;
+        cout << "Execution time: " << elapsed.count() << endl;
+        cout << "********************************\n"
+             << endl;
     }
-
-    for (int i = 0; i < numberOfThreads; i++)
-    {
-        // Must wait for each thread to finish
-        v[i].join();
-    }
-
-    // Ending to measure time
-    auto end = chrono::system_clock::now();
-
-    chrono::duration<double> elapsed = end - start;
-
-    cout << "\n************ RESULT ************" << endl;
+    cout << "\n************ RESULTADO FINAL ************\n"
+         << endl;
     cout << "Number of elements: " << NUM_ELEMENTS << endl;
-    cout << "Cumulative sum: " << cumulativeSum << endl;
     cout << "Threads quantity: " << numberOfThreads << endl;
-    cout << "Execution time: " << elapsed.count() << endl;
+    cout << "Average execution time: " << totalTime / numberOfExecutions << endl;
     cout << "********************************\n"
          << endl;
 
